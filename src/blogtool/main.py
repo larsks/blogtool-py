fdc1e6aaff53ed44afb6aa266ebd1017450c17a3
Lars Kellogg-Stedman <lars@oddbit.com>
2021-02-08 16:45:03 -0500

    store main branch as git.Head instead of string

--- src/blogtool/main.py
+++ src/blogtool/main.py
@@ -18,21 +18,27 @@ LOG = logging.getLogger(__name__)
 @dataclass
 class Context:
     repo: git.Repo = None
-    main_branch: str = None
-    post_directory: str = None
+    main_branch: git.Head = None
+    post_directory: Path = None
 
 
-@click.group()
+@click.group(context_settings={'auto_envvar_prefix': 'BLOGTOOL_'})
 @click.option('--verbose', '-v', count=True)
-@click.option('--main-branch', '-M', default='master')
+@click.option('--main-branch', '-M', 'main_branch_name', default='master')
 @click.option('--post-directory', '-P', default='post/', type=Path)
 @click.pass_context
-def main(ctx, verbose, main_branch, post_directory):
+def main(ctx, verbose, main_branch_name, post_directory):
     try:
         repo = git.Repo(search_parent_directories=True)
     except git.exc.InvalidGitRepositoryError:
         raise click.ClickException(
             'blogtool must be run from inside a git repository')
+
+    try:
+        main_branch = repo.refs[main_branch_name]
+    except IndexError:
+        raise click.ClickException(
+            f'no branch named {main_branch_name} in this repository')
 
     ctx.obj = Context(
         repo=repo,
